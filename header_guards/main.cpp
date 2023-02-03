1a839fb8c69add69eb3862c814510442f47e0f04
Osman El-Ghotmi <osman.elghotmi@gmail.com>
2023-02-03 18:21:52 -0500

    header guards

---
+++ header_guards/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "square.h"
+#include "cube.h"
+
+int main(){
+  const int x = 5;
+  auto x_squared = square(x);
+  auto x_cubed = cube(x);
+
+  std::cout << "x*x = " << x_squared << std::endl;
+  std::cout << "x*x*x = " << x_cubed << std::endl;
+
+  return 0;
+}
---
+++ header_guards/square.h
@@ -0,0 +1,7 @@
+#ifndef SQUARE_H
+#define SQUARE_H
+template<typename Scalar_type>
+auto square(Scalar_type x){
+  return x*x;
+}
+#endif
