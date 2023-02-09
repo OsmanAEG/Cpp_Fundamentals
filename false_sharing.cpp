c2d5e5844cdbb6d09234a38e3e0ca1070e40c97c
Osman El-Ghotmi <osman.elghotmi@gmail.com>
2023-02-08 21:51:00 -0500

    false sharing updates

--- false_sharing.cpp
+++ false_sharing.cpp
@@ -22,7 +22,7 @@ void one_thread_execution(){
   }
 }
 
-void problematic_thread_execution(){
+void multiple_threads_single_variable(){
   std::atomic<int> A;
   std::vector<std::thread> threads;
   int N = 5000;
@@ -40,7 +40,48 @@ void problematic_thread_execution(){
   }
 }
 
+void multiple_threads_multiple_variables_same_cache_line(){
+  std::vector<std::thread> threads;
+  int N = 5000;
+  int n = 6;
+  std::vector<std::atomic<int>> As(n);
+
+  for(int i = 0; i < n; ++i){
+    std::thread thread_i([&](){increment(As[i], N);});
+    threads.push_back(std::move(thread_i));
+  }
+
+  for(auto& thread : threads){
+    thread.join();
+  }
+}
+
+struct alignas(64) aligned_type{
+  std::atomic<int> A;
+};
+
+void multiple_threads_multiple_variables_different_cache_line(){
+  std::vector<std::thread> threads;
+  int N = 5000;
+  int n = 6;
+  std::vector<aligned_type> As(n);
+
+  for(int i = 0; i < n; ++i){
+    std::thread thread_i([&](){increment(As[i].A, N);});
+    threads.push_back(std::move(thread_i));
+  }
+
+  for(auto& thread : threads){
+    thread.join();
+  }
+}
+
 int main(){
+  one_thread_execution();
+  one_thread_execution();
+  multiple_threads_single_variable();
+  multiple_threads_multiple_variables_same_cache_line();
+  multiple_threads_multiple_variables_different_cache_line();
   return 0;
 }
 
